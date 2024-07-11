488ca7f73aaeb9cef44a1c91b4ba4718634c3383
Eduardo Lopes <eduardotorreslopes@gmail.com>
2024-07-11 20:56:35 -0300

    feat: create file to index the components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { RightHeader } from './RightHeader'
+import { MenuItem } from './MenuItem'
+import { Total } from './Total'
+import { CartItem } from './CartItem'
+
+export { RightHeader, MenuItem, Total, CartItem }
