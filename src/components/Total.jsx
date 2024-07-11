556c5353807b72e9e870f0538ff5a7b5faacc762
Eduardo Lopes <eduardotorreslopes@gmail.com>
2024-07-11 20:56:58 -0300

    feat: create Total component

---
+++ src/components/Total.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { styles } from '../styles/shoppingCart'
+
+export const Total = ({ cart }) => {
+  const total = cart.reduce((acc, item) => acc + item.price, 0)
+  return (
+    <View style={styles.totalContainer}>
+      <Text style={styles.totalText}>Total</Text>
+      <Text style={styles.totalAmount}>${total.toFixed(2)}</Text>
+    </View>
+  )
+}
